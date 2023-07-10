4821911d0f6a68d872297cbc77d0dbbe67fe4542
huy.bc <huy.bc@aptechlearning.edu.vn>
2023-07-10 10:29:05 +0700

    commit

---
+++ DEMO_PROJECT_C2108G3_Delivery/Controllers/AdminPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DEMO_PROJECT_C2108G3_Delivery.Controllers
+{
+    public class AdminPageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DEMO_PROJECT_C2108G3_Delivery/Data/Migrations/20230707040920_ChangeDB.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DEMO_PROJECT_C2108G3_Delivery.Data.Migrations
+{
+    public partial class ChangeDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
